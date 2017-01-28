0e2e76510acad6a8c97c68b9ee0a264b522b9628
DikshaPoulenkar <dikshapoulenkar@gmail.com>
2017-01-28 10:19:20 +0530

    Update expt1_1a.cpp
    stream added to the code

--- expt1_1a.cpp
+++ expt1_1a.cpp
@@ -5,6 +5,7 @@ struct student
 {
 	char name[7];
 	int rollno;
+	char stream[20];
 }s1;
 
 
@@ -12,8 +13,8 @@ void output(student *s2);
 	
 int main()
 {
-	printf("Enter the name and roll no. of student");
-	scanf("%s %d",s1.name,&s1.rollno);
+	printf("Enter the name and roll no. , and stram of student");
+	scanf("%s %d %s",s1.name,&s1.rollno,&s1.stream);
 	output(&s1);
 	return 0;
 }
@@ -23,6 +24,8 @@ void output(student *s2)
 {
 	strcpy(s2 -> name,"diksha");
 	s2 -> rollno= 36;
+	strcpy(s2->stream,"Science");
 	printf("Name:%s",s2->name);
 	printf("\nRoll no.:%d",s2->rollno);
+	printf("\nStream:%s",s2->stream);
 }
